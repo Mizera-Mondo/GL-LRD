87429849280ad323e1e30742b56467680438e7f0
Zian_ <Zian_@DESKTOP-0K932DU>
2023-05-22 04:34:05 +0800

    No converge research

---
+++ functions/GL_NO_LRD.m
@@ -0,0 +1,144 @@
+function [X, L] = GL_NO_LRD(Y, R, k, alpha, beta)
+%LR-DGI Solve 1/2||D(Y - X)||_F^2 + alpha*Tr{D(X)'*L*D(X)} +
+%beta/2||L||_F^2
+% k is the maximum rank of estimated signal matrix X
+X = Y;
+
+
+% Random vaild initial value of A
+[n, T] = size(Y);
+B = [zeros(T - 1, 1) eye(T - 1); ...
+    zeros(1, T)];
+D = @(X) X - R*X*B;
+A = rand(n, n);
+A = A - diag(diag(A));
+A = A + A';
+A = n*A./sum(A, 'all');
+L = diag(sum(A)) - A;
+
+tol = 1e-3;
+iter = 1;
+maxIter = 1000;
+isConverge = false;
+isMaxIter = false;
+
+while ~isConverge && ~isMaxIter
+    L_old = L;
+    X_old = X;
+    % Optimizing L a.k.a. A
+    DX = D(X);
+    M1 = DX*DX';
+    M2 = repmat(diag(M1), 1, n);
+    M = M2 + M2' - 2*M1;
+    A = solveSubA(M, alpha, beta);
+
+    L = diag(sum(A)) - A;
+
+    % Optimizing X
+    X = solveSubX(Y, L, R, B, alpha, k);
+
+    isConverge = norm(L_old - L, 'fro')/(norm(L_old, 'fro') + 1e-10) < tol ...
+            && norm(X_old - X, 'fro')/(norm(X_old, 'fro') + 1e-10) < tol;
+    isMaxIter = iter >= maxIter;
+    iter = iter + 1;
+    disp(num2str(iter));
+end
+end
+
+function A = solveSubA(M, alpha, beta)
+%solveSubA solve the sub-problem ||A||_F^2 + alpha/beta*Tr{AM}
+[n, ~] = size(M);
+on = ones(n, 1);
+ze = zeros(n, 1);
+cvx_begin quiet
+    variable A(n, n) symmetric nonnegative
+    minimize square_pos(norm(A, 'fro')) + alpha/beta*trace(A*M)
+    subject to
+        diag(A) == ze;
+        on'*A*on == n;
+cvx_end
+end
+
+function X = solveSubX(Y, L, R, B, alpha, k)
+%solveSubX solve the sub-problem 1/2||D(Y-X)||_F^2 +
+%alpha*Tr{D(X)'*L*D(X)}, s.t. rank(X) <= k, using ADMM
+
+[n, ~] = size(Y);
+P = randn(n, k); % random initial value
+Q = (P\Y)';
+X = Y;
+Th = X - P*Q';
+rho = 1;
+ita = 1.05;
+rhoMax = 10;
+
+tolSqu = 1e-6;
+tol = sqrt(tolSqu);
+maxIter = 1000;
+iter = 1;
+isConverge = false;
+isMaxIter = false;
+
+while ~isConverge && ~isMaxIter
+    % Debug output
+    X_old = X;
+    P_old = P;
+    Q_old = Q;
+    % Update of X
+    X_ = P*Q' - Th/rho;
+    X = updateX(X, Y, L, X_, R, B, alpha, rho);
+    % Update of P
+    %P = (X + 1/rho*Th)*Q/(Q'*Q);
+    % Update of Q
+    %Q = (X' + 1/rho*Th')*P/(P'*P);
+    % Update of Th and rho
+    %Th = Th + rho*(X - P*Q');
+    %rho = min([ita*rho, rhoMax]);
+    % Terminating Condition Check
+    if norm(X_old - X, 'fro')/(norm(X_old, 'fro') + 1e-10) < tol ...
+            && norm(P_old - P, 'fro')/(norm(P_old, 'fro') + 1e-10) < tol ...
+            && norm(Q_old - Q, 'fro')/(norm(Q_old, 'fro') + 1e-10) < tol
+        isConverge = true;
+    end
+    isMaxIter = iter >= maxIter;
+    iter = iter + 1;
+end
+end
+
+function X = updateX(X, Y, L, X_, R, B, alpha, rho)
+%updateX solves the problem: 1/2||D(Y - X)||_F^2 + alpha*Tr{D(X)'*L*D(X)} +
+%rho/2*||X - X_||_F^2
+%
+% Where X_ = P*Q' - Th/rho
+
+D = @(X) X - R*X*B;
+DY = D(Y);
+L_ = eye(size(L)) + 2*alpha*L;
+H = DY - R'*DY*B' + rho*X_;
+
+tarFun = @(X) 1/2*norm(DY - D(X), 'fro')^2 + alpha*trace(D(X)'*L*D(X)) + rho/2*norm(X - X_, 'fro');
+grad = @(X) L_*X + R'*L_*R*X*(B*B') - L_*R*X*B - R'*L_*X*B' + rho*X - H;
+
+% TODO: Armijo update
+c = 1e-2;
+a = 0.9;
+iter = 1;
+maxIter = 100;
+isArmijoNod = false;
+isMaxIter = false;
+
+gradX = grad(X);
+while ~isArmijoNod && ~isMaxIter
+    deltaX = -1*a.^(iter)*gradX;
+    expectDescent = c*trace(gradX'*deltaX);
+    realDescent = tarFun(X + deltaX) - tarFun(X);
+    isArmijoNod = realDescent < expectDescent;
+    isMaxIter = iter >= maxIter;
+    iter = iter + 1;
+end
+if isArmijoNod
+    X = X + deltaX;
+else
+    disp('X unchanged due to non-decreasing within tolerance.');
+end
+end
---
+++ functions/LRD.m
@@ -0,0 +1,39 @@
+function [P, Q] = LRD(X, k)
+%LRD Low-rank decomposition of matrix X = P*Q' with maximum rank K with ADMM
+[n, T] = size(X);
+P = randn(n, k);
+Q = randn(T, k);
+Th = X - P*Q';
+
+rho = 1;
+ita = 1.05;
+rhoMax = 10;
+tol = 1e-3;
+
+iter = 1;
+maxIter = 1000;
+isAdmmConverge = false;
+isMaxIter = false;
+
+while ~isAdmmConverge && ~isMaxIter
+    P_old = P;
+    Q_old = Q;
+
+    cvx_begin quiet
+        variable P(n, k)
+        minimize trace(Th'*(X - P*Q')) + rho/2*norm(X - P*Q', 'fro')
+    cvx_end
+
+    cvx_begin quiet
+        variable Q(T, k)
+        minimize trace(Th'*(X - P*Q')) + rho/2*norm(X - P*Q', 'fro')
+    cvx_end
+    Th = Th + rho*(X - P*Q');
+    rho = min([ita*rho, rhoMax]);
+    isAdmmConverge = norm(P - P_old, 'fro')/norm(P_old) < tol ...
+                  && norm(Q - Q_old, 'fro')/norm(Q_old) < tol;
+    isMaxIter = iter >= maxIter;
+    iter = iter + 1;
+
+end
+
